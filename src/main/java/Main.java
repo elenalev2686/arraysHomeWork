a53bec5b5d563066bdb00ddfe0193d01f02eba80
elenalev2686 <elenalev2686@gmail.com>
2023-08-15 20:40:09 +0700

    changed src

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import ru.netology.stats.arrayshomework.service.StatsService;
 
 import java.util.Arrays;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         long[] sales = {
                 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
