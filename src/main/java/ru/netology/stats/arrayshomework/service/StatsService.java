3b8ac530cd228882353f0aefd802c1713bda8b33
elenalev2686 <elenalev2686@gmail.com>
2023-08-16 07:02:09 +0700

    changed src

--- src/main/java/ru/netology/stats/arrayshomework/service/StatsService.java
+++ src/main/java/ru/netology/stats/arrayshomework/service/StatsService.java
@@ -2,27 +2,32 @@ package ru.netology.stats.arrayshomework.service;
 
 public class StatsService {
     public long getSalesAmount(long[] sales) {
-        long salesAmount = 0; int i;
-        for (i=0; i<12; i++)
+        long salesAmount = 0;
+        int i;
+        for (i = 0; i < 12; i++) {
             salesAmount = salesAmount + sales[i];
-
+        }
         return salesAmount;
     }
-    public long getAverageFromSales(long[] sales) {
-        long salesAmount = 0; int i;
-        for (i=0; i<12; i++)
-            salesAmount = salesAmount + sales[i];
 
-        return salesAmount /12;
+    public long getAverageFromSales(long[] sales) {
+        long salesAmount = 0;
+        int i;
+        for (i = 0; i < 12; i++) {
+            salesAmount = salesAmount + sales[i];
+        }
+        return salesAmount / 12;
     }
+
     public long getMaxSales(long[] sales) {
         int maxMonth = 0;
-        for (int i = 0; i < sales.length; i++)
+        for (int i = 0; i < sales.length; i++) {
             if (sales[i] >= sales[maxMonth])
                 maxMonth = i;
-
+        }
         return maxMonth++;
     }
+
     public long getMinSales(long[] sales) {
         int minMonth = 0;
         for (int i = 0; i < sales.length; i++) {
@@ -31,6 +36,7 @@ public class StatsService {
         }
         return minMonth++;
     }
+
     public long getAboveAverageSales(long[] sales) {
         int aboveAverageMonth = 0;
         long averageFromSales = getAverageFromSales(sales);
@@ -42,6 +48,7 @@ public class StatsService {
         }
         return aboveAverageMonth;
     }
+
     public long getBelowAverageSales(long[] sales) {
         int belowAverageMonth = 0;
         long averageFromSales = getAverageFromSales(sales);
--- src/test/java/StatsServiceTest.java
+++ src/test/java/StatsServiceTest.java
@@ -10,8 +10,9 @@ public class StatsServiceTest {
                 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 180;
         long actual = service.getSalesAmount(sales);
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void shouldGetAverageFromSales() {
         StatsService service = new StatsService();
@@ -19,8 +20,9 @@ public class StatsServiceTest {
                 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 15;
         long actual = service.getAverageFromSales(sales);
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void shouldGetMaxSales() {
         StatsService service = new StatsService();
@@ -28,8 +30,9 @@ public class StatsServiceTest {
                 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 7;
         long actual = service.getMaxSales(sales);
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void shouldGetMinSales() {
         StatsService service = new StatsService();
@@ -37,8 +40,9 @@ public class StatsServiceTest {
                 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 8;
         long actual = service.getMinSales(sales);
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void shouldGetAboveAverageSales() {
         StatsService service = new StatsService();
@@ -46,8 +50,9 @@ public class StatsServiceTest {
                 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 5;
         long actual = service.getAboveAverageSales(sales);
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void shouldGetBelowAverageSales() {
         StatsService service = new StatsService();
@@ -55,6 +60,6 @@ public class StatsServiceTest {
                 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 5;
         long actual = service.getBelowAverageSales(sales);
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
 }
